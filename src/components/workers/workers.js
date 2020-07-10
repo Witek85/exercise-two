30a877ea242f87ce9b16d87325d1d36cfaf7395a
Witek85 <witold.szklarski@gmail.com>
2020-07-10 12:25:03 +0200

    workers grid

--- src/components/workers/workers.js
+++ src/components/workers/workers.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './workers.css';
 
-function Workers() {
-  return (
-    <div className="workers">
-    </div>
-  );
+class Workers extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {workers: [
+      {id:1, name:'Jan', surname: 'Kowalski'},
+      {id:2, name:'Jan', surname: 'Kowalski'},
+      {id:3, name:'Jan', surname: 'Kowalski'},
+      {id:4, name:'Jan', surname: 'Kowalski'},
+      {id:5, name:'Jan', surname: 'Kowalski'},
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="workers">
+        <span className="workers-top">Pracownicy</span>
+        {this.state.workers.map(worker => 
+          <>
+            <span className="worker-left">{worker.name}</span>
+            <span className="worker-right">{worker.surname}
+            </span>
+          </>)
+        }
+      </div>
+    )
+  }
 }
 
 export default Workers;
